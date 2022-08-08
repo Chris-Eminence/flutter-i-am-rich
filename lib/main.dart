135209d4f2fdfb4301ed631a070875069d2459c7
chris-eminence <emichris380@gmail.com>
2022-08-09 00:42:29 +0100

    image from image asset added and pubspec.yaml file configured

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80'),
+            image: AssetImage('images/android_icon.png'),
           ),
         ),
       ),
