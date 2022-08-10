f7bc65688f98571f8179de96a3cbd7ab6df7b002
chris-eminence <emichris380@gmail.com>
2022-08-11 00:10:17 +0100

    Working with single child layout widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Trying());
+}
+
+class Trying extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Text('I am rich'),
-          //changing the color of the appBar
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/android_icon.png'),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 200.0,
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            color: Colors.white,
+            child: const Text('This is my first Flutter App'),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
