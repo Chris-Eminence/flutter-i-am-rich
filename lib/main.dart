14a0bc8994fd310f37acedfb8638bf3081c17b5c
chris-eminence <emichris380@gmail.com>
2022-08-11 23:05:14 +0100

    Worked with CircleAvatar image widget, Card and ListTiles

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class Trying extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: const [
               CircleAvatar(
-                radius: 40.0,
+                radius: 100.0,
                 backgroundColor: Colors.blue,
                 backgroundImage: NetworkImage(
                     'https://scontent.flos1-1.fna.fbcdn.net/v/t39.30808-6/298329476_1888416691507681_4037601813364962393_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeG18Fv0ArE9osXCgMqw6TxjKFNcEIlMz6EoU1wQiUzPoVVYGgTAJR7HOqQTvaNN5b2VgvrHUEsyTIJq7KEkn2zq&_nc_ohc=g86nC3YzJi0AX-ZlOjO&tn=8pwKJyTEryHZ5ToV&_nc_zt=23&_nc_ht=scontent.flos1-1.fna&oh=00_AT9gQgpYft4iAeeMJ0a4nrmpW7gUVdR7IQvdIfmdwY0SeA&oe=62F9AECE'),
@@ -25,25 +25,30 @@ class Trying extends StatelessWidget {
                 height: 5.0,
               ),
               Text(
-                'Chris Emediong',
+                'Chris Eminence',
                 style: TextStyle(
-                  fontSize: 16.0,
+                  fontSize: 35.0,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
               ),
               SizedBox(
-                height: 20.0,
+                height: 30.0,
+                width: 250.0,
                 child: Divider(
-                  color: Colors.black45,
+                  color: Colors.white70,
                 ),
               ),
               Text(
                 'FLUTTER DEVELOPER',
                 style: TextStyle(
-                  letterSpacing: 2.5,
+                  letterSpacing: 3.5,
                   color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
+              ),
+              SizedBox(
+                height: 10.0,
               ),
               Card(
                 color: Colors.white,
@@ -57,7 +62,7 @@ class Trying extends StatelessWidget {
                     color: Colors.deepPurpleAccent,
                   ),
                   title: Text(
-                    '+234 80 123 456',
+                    '+234 90 312 572 56',
                     style: TextStyle(
                       color: Colors.black45,
                     ),
